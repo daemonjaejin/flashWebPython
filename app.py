eb1e2e4369aaa3def1e994b817eae4b978a92619
이재진 <starcx@naver.com>
2024-02-26 13:24:44 +0900

    라우팅

--- app.py
+++ app.py
@@ -4,15 +4,29 @@ import random
 app = Flask(__name__)
 
 
-@app.route('/')
+# @app.route('/')
 # def hello_world():  # put application's code here
 #     return 'Hello'
 
 # 랜덤 함수를 문자로 변환해서 리턴
+@app.route('/')
 def index():
-    return 'random : <strong>'+str(random.random())+'</strong>'
+    # return 'random : <strong>'+str(random.random())+'</strong>'
+    return 'Welcome'
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+# @app.route('/read/<int:post_id>/')
+# @app.route('/read/<path:subpath>/')
+@app.route('/read/<id>/')
+def read(id):
+    print(id)
+    return 'Read '+str(id)
 
 # if __name__ == '__main__':
 #     app.run(port=5001, debug=True)
 
 app.run(port=5001, debug=True)
+
