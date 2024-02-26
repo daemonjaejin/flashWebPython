c47a4367797b9771978679cd22f96682061865f7
이재진 <starcx@naver.com>
2024-02-26 12:39:34 +0900

    flask를 사용하는 이유

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello'
+# def hello_world():  # put application's code here
+#     return 'Hello'
 
+# 랜덤 함수를 문자로 변환해서 리턴
+def index():
+    return 'random : <strong>'+str(random.random())+'</strong>'
 
-if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+# if __name__ == '__main__':
+#     app.run(port=5001, debug=True)
+
+app.run(port=5001, debug=True)
