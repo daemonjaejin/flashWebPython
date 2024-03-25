bc900189187c3f8e3fbde55b3294d5146b4076ce
이재진 <starcx@naver.com>
2024-03-25 15:37:09 +0900

    20240325-03

---
+++ templates/20240325-03.py
@@ -0,0 +1,85 @@
+d = list(range(1, 10, 3))
+
+d[0] = 100
+
+print(d)
+
+e = list(range(4))
+
+print(e)
+
+e.append(4)
+
+print(e)
+
+e.append(100)
+
+print(e)
+
+f = list(range(5))
+
+print(f)
+
+print(f[0])
+
+del f[0]
+
+print(f[0])
+
+g = list(range(20))
+
+print(g)
+
+print(g[0:5])
+
+print(g[:5])
+
+print(g[10:15])
+
+print(g[-1])
+
+print(g[-5:-2])
+
+print(g[:-5])
+
+print(g[-5:])
+
+print("==================================")
+
+h = [1, 2, 3]
+
+h1, h2, h3 = h
+
+print(h1)
+
+print(h2)
+
+print(h3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
